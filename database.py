0e1d61cb628749774f55c780405980690a259214
Shubhangi <73872712+shubs99@users.noreply.github.com>
2023-03-20 08:10:38 +0000

    aesthetic improvements

--- database.py
+++ database.py
@@ -19,3 +19,24 @@ def load_jobs_from_db():
       jobs_list.append(dict(zip(column_names, row)))
       print(jobs_list)
     return jobs_list
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
+  rows = []
+  for row in result.all():
+    rows.append(row._mapping)
+  if len(rows) == 0:
+    return None
+  else:
+    return dict(rows[0])
+
+
+# rows = []
+# for row in result.all():
+#   rows.append(row._mapping)
+# if len(rows) == 0:
+#   return None
+# else:
+#   return [dict(row) for row in rows]
